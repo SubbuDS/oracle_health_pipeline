c6e513293b13bd4e5895386d83599dfd9e3d76d3
subba rao <35118306+SubbuDS@users.noreply.github.com>
2026-04-16 13:23:52 -0500

    fix: use dynamic driver host IP for K8s client mode

--- scripts/run_silver_batch.sh
+++ scripts/run_silver_batch.sh
@@ -5,9 +5,12 @@ echo "Starting EHR Silver Batch Job on Kubernetes..."
 
 SPARK_HOME="${SPARK_HOME:-/spark}"
 PIPELINE_HOME="${PIPELINE_HOME:-/pipeline}"
-MAC_IP="192.168.1.84"
 
-export MINIO_ENDPOINT="http://${MAC_IP}:9000"
+# Get the IP of the machine running the driver (Mac or Jenkins container)
+DRIVER_HOST=$(hostname -I | awk '{print $1}')
+echo "Driver host: $DRIVER_HOST"
+
+export MINIO_ENDPOINT="http://host.docker.internal:9000"
 export MINIO_ACCESS_KEY="minioadmin"
 export MINIO_SECRET_KEY="minioadmin"
 export MINIO_BUCKET="spark-data"
@@ -23,8 +26,9 @@ $SPARK_HOME/bin/spark-submit \
   --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
   --conf spark.kubernetes.container.image=ehr-spark:latest \
   --conf spark.kubernetes.container.image.pullPolicy=Never \
-  --conf spark.driver.host=${MAC_IP} \
-  --conf spark.hadoop.fs.s3a.endpoint=http://${MAC_IP}:9000 \
+  --conf spark.driver.host=${DRIVER_HOST} \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.hadoop.fs.s3a.endpoint=http://host.docker.internal:9000 \
   --conf spark.hadoop.fs.s3a.access.key=minioadmin \
   --conf spark.hadoop.fs.s3a.secret.key=minioadmin \
   --conf spark.hadoop.fs.s3a.path.style.access=true \
@@ -33,7 +37,7 @@ $SPARK_HOME/bin/spark-submit \
   --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
   --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
   --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
-  --conf spark.executorEnv.MINIO_ENDPOINT=http://${MAC_IP}:9000 \
+  --conf spark.executorEnv.MINIO_ENDPOINT=http://host.docker.internal:9000 \
   --conf spark.executorEnv.MINIO_ACCESS_KEY=minioadmin \
   --conf spark.executorEnv.MINIO_SECRET_KEY=minioadmin \
   --conf spark.executorEnv.MINIO_BUCKET=spark-data \
